42e13d59889e4511af6b36c8fb8090367eb7dbd5
Jeremy_134 <u202022237@upc.edu.pe>
2024-06-18 10:28:50 -0500

    init

---
+++ src/main/java/com/upc/saveup/card_microservice/CardMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.upc.saveup.card_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/saveup/card_microservice/CardMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.saveup.card_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
